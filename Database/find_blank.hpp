c2b35a61f0e71e8d662b9c45950685da76000615
Hanyuezhuohua <58478765+Hanyuezhuohua@users.noreply.github.com>
2020-06-20 09:52:24 +0800

    Update find_blank.hpp

--- Database/find_blank.hpp
+++ Database/find_blank.hpp
@@ -9,7 +9,7 @@ public:
 	const off_t K = 8192;
 	int num;
 	off_t size;
-	// off_t *blank;
+	off_t *blank;
 	char *index_file;
 public:
 	find_blank() {}
@@ -18,12 +18,12 @@ public:
 		strcpy(index_file, in_file);
 		FILE *file = fopen(index_file, "rb+");
 		if (!file) {
-			// num = 0;
+			num = 0;
 			size = 3 * sizeof(off_t);
-			// blank = nullptr;
+			blank = nullptr;
 			save_info();
 		} else {
-			// blank = nullptr;
+			blank = nullptr;
 			fclose(file);
 			load_info();
 			save_info();
@@ -31,9 +31,9 @@ public:
 	}
 
 	~find_blank() {
-		// if (blank){
-		// 	delete blank;
-		// } 
+		if (blank){
+			delete blank;
+		} 
 		if (index_file) delete index_file; 
 	}
 
@@ -42,13 +42,13 @@ public:
 		strcpy(index_file, in_file);
 		FILE *file = fopen(index_file, "rb+");
 		if (!file) {
-			// num = 0;
+			num = 0;
 			size = 3 * sizeof(off_t);
-			// blank = nullptr;
+			blank = nullptr;
 			save_info();
 		}
 		else {
-			// blank = nullptr;
+			blank = nullptr;
 			fclose(file);
 			load_info();
 			save_info();
@@ -56,51 +56,51 @@ public:
 	}
 
 	void save_info() {
-		// buffer_write(&num, 0, sizeof(int), index_file);
+		buffer_write(&num, 0, sizeof(int), index_file);
 		buffer_write(&size, sizeof(int), sizeof(off_t), index_file);
-		// if (num) {
-		// 	for (int i = 0; i < num; ++i)
-		// 		buffer_write(&blank[i], sizeof(int) + sizeof(off_t) * i, sizeof(off_t), index_file);
-		// }
+		if (num) {
+			for (int i = 0; i < num; ++i)
+				buffer_write(&blank[i], sizeof(int) + sizeof(off_t) * i, sizeof(off_t), index_file);
+		}
 	}
 
 	void load_info() {
-		// buffer_read(&num, 0, sizeof(int), index_file);
+		buffer_read(&num, 0, sizeof(int), index_file);
 		buffer_read(&size, sizeof(int), sizeof(off_t), index_file);
-		// if (num) {
-		// 	blank = new off_t[num + 1];
-		// 	for (int i = 0; i < num; ++i)
-		// 		buffer_read(&blank[i], sizeof(int) + sizeof(off_t) * i, sizeof(off_t), index_file);
-		// }
+		if (num) {
+			blank = new off_t[num + 1];
+			for (int i = 0; i < num; ++i)
+				buffer_read(&blank[i], sizeof(int) + sizeof(off_t) * i, sizeof(off_t), index_file);
+		}
 	}
 
 	off_t m_alloc() {
 		load_info();
-		// if (num == 0) {
+		if (num == 0) {
 			off_t tmp = size;
 			size += K;
 			save_info();
 			return tmp;
-		// } else {
-		// 	--num;
-		// 	save_info();
-		// 	return blank[num];
-		// }
+		} else {
+			--num;
+			save_info();
+			return blank[num];
+		}
 	}
 
 	void m_delete(off_t pos) {
 		load_info();
-		// if (num == 0) {
-		// 	num = 1;
-		// 	blank = new off_t[1];
-		// 	blank[0] = pos;
-		// } else {
-		// 	blank[num++] = pos;
-		// }
+		if (num == 0) {
+			num = 1;
+			blank = new off_t[1];
+			blank[0] = pos;
+		} else {
+			blank[num++] = pos;
+		}
 		save_info();
-		// num = 0;
-		// size = invalid_off;
-		// delete blank;
-		// blank = nullptr;
+		num = 0;
+		size = invalid_off;
+		delete blank;
+		blank = nullptr;
 	}
 };
