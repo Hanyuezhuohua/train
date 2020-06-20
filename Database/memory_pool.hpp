43d26b5402b2e78a38b9a0bfe7ce917b4ca67c75
Hanyuezhuohua <58478765+Hanyuezhuohua@users.noreply.github.com>
2020-06-20 09:55:26 +0800

    Update memory_pool.hpp

--- Database/memory_pool.hpp
+++ Database/memory_pool.hpp
@@ -11,7 +11,7 @@ using ull = unsigned long long;
 
 const ull hash_seed = 233;
 
-const size_t bitsize = 1024;
+const size_t bitsize = 2048;
 
 extern bool cleaned;
 
@@ -78,7 +78,7 @@ struct unit {
 	}
 };
 
-const size_t magic = 33331, K = 1024;
+const size_t magic = 33331, K = 2048;
 
 class buffer_pool {
 private:
